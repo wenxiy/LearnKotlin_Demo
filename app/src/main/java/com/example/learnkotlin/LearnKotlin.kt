ee8b41f4b4e6184100e2d37cc379720ab7adb92c
wenxiy <193367189@qq.com>
2020-09-22 15:42:59 +0800

    Update LearnKotlin.kt

--- app/src/main/java/com/example/learnkotlin/LearnKotlin.kt
+++ app/src/main/java/com/example/learnkotlin/LearnKotlin.kt
@@ -24,8 +24,33 @@ fun largerName(num1: Int, num2: Int): Int {
     return value
 }
 
-fun largerName1(num1: Int, num2: Int): Int = if (num1 > num2){
+fun largerName1(num1: Int, num2: Int): Int = if (num1 > num2) {
     num1
-} else{
+} else {
     num2
 }
+
+fun getScore(name: String) = if (name == "Tom") {
+    86
+} else if (name == "Jim") {
+    77
+} else if (name == "Jack") {
+    95
+} else if (name == "Lily") {
+    100
+} else {
+    0
+}
+
+/**
+ * when和if一样，是可以拥有返回值的。而且对于When来说，也是可以写在语法糖中的。以下这个例子是
+ * kotlin的精确匹配
+ */
+fun getScore2(name: String) = when (name) {
+    "Tom" -> 86
+    "Jim" -> 77
+    "Jack" -> 95
+    "Lily" -> 100
+    else -> 0
+}
+fun CheckNum(num:Number)
