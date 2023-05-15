f09e5eb679ca792f9927437e358cccba0b0f544c
温鑫 <49605281+wenxiy@users.noreply.github.com>
2023-05-15 11:28:04 +0800

    lambda study
    lambda study

--- otherproject/app/src/main/java/com/example/myapplication/KTLambda.kt
+++ otherproject/app/src/main/java/com/example/myapplication/KTLambda.kt
@@ -38,12 +38,34 @@ fun main() {
     val world = charArrayOf('w', 'o', 'r', 'l', 'd')
     val append2 = append('h', 'e', 'l', 'l', 'o', ' ', *world, start = 1)
     println("append2： $append2")
+    //调用具有方法类型参数的方法
+    val numbers = arrayOf(1, 2, 3, 4)
+    //具名参数的调用，即用参数名action= 来赋值
+    transform(numbers, action = { index, element ->
+        index * element //闭包中最后一行表达式，计算后的结果，就是action函数的返回值
+    })//也可以省略方法，因为action是他最后一个参数
+    for (numbers in numbers) {
+        println("transform: $numbers")
+    }
+    //lambda隐形参数it
+    numbers.forEach {
+        println(it)
+    }
+
+    val list = arrayListOf<Int>(1, 2, 3, 4, 5, 6)
+    list.forEachIndexed(action = { index: Int, element: Int ->
+        println("forEachindexed: $index->$element")
+    })
+    list.forEachIndexed { index: Int, element: Int ->
+        println("forEachindexed2:$index->$element")
+    }
+
 }
 
 //不定长参数传输
 //一个方法中只能有一个vararg参数（不定参数）
 //如果vararg参数不是最后一个参数，必须要用具名参数语法，给后面的参数传递值
-fun append(vararg str: Char,start: Int): String {
+fun append(vararg str: Char, start: Int): String {
     val result = StringBuffer()
     for (char in str) {
         result.append(char)
@@ -64,6 +86,16 @@ fun read2(offset: Int = 0, start: Int, action: () -> String) {
     println("read2:$ret")
 }
 
+// lambda表达式作为方法中的参数的时候/定义transform方法，可以对数组中的元素进行变换
+// array:要求传入一个数组，元素类型为Int整型类型
+// action:要求传入的是一个方法，接受两个参数下标index，元素element
+// 要求返回变换后的元素，会替换老的元素
+fun transform(array: Array<Int>, action: (index: Int, element: Int) -> Int) {
+    for (index in array.indices) {//indices是通过索引的方式遍历数组
+        val newValue = action(index, array[index])
+        array[index] = newValue
+    }
+}
 
 //普通类方法
 class Person {
