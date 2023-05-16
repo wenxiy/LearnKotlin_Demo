b3fb574044fa2512c79405b5a35fc3c6b70f822f
温鑫 <49605281+wenxiy@users.noreply.github.com>
2023-05-16 21:52:59 +0800

    11
    111

--- otherproject/app/src/main/java/com/example/myapplication/KTCondition.kt
+++ otherproject/app/src/main/java/com/example/myapplication/KTCondition.kt
@@ -14,7 +14,32 @@ fun maxOf(a: Int, b: Int): Int {
 }
 
 fun maxOf2(a: Int, b: Int): Int {
+    //三目运算
     return if (a > b) a else b
 }
-//111
 
+fun eval(number: Number) {
+    //is 是判断是否是某一种类型
+    if (number is Int) {
+        println("this is int number")
+    } else if (number is Double) {
+        println("this is double number")
+    } else if (number is Float) {
+        println("this is Float number")
+    } else if (number is Short) {
+        println("this is Short number")
+    } else {
+        throw IllegalAccessException("invalid argument")
+    }
+}
+
+//when 是带有返回值的
+//when 将它的参数与所有的分支条件顺序比较，直到某个分支满足条件
+fun eval2(number: Number): String = when (number) {
+    200f -> "the number is 200f"
+    is Int -> "this is int number"
+    is Double -> "this is Double number"
+    is Float -> "this is float number"
+    is Long -> "this is byte number"
+    else -> "invalid number"
+}
---
+++ otherproject/app/src/main/java/com/example/myapplication/KTLoop.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+//Kotlin 循环控制
+fun main() {
+    val items = listOf("java", "kotlin", "android")
+    //for-in
+    for (item in items) {
+        println(item)
+    }
+    //forEach
+    items.forEach { item ->
+        println("forEach:${item}")
+    }
+
+    items.forEachIndexed { index, item->
+        println("forEachIndexed: ${index},${item}")
+    }
+}
