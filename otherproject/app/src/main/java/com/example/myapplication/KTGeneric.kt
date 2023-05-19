883982bfff9b1e2259263c21d74d787ac7347bea
温鑫 <49605281+wenxiy@users.noreply.github.com>
2023-05-19 21:40:28 +0800

    11
    11

---
+++ otherproject/app/src/main/java/com/example/myapplication/KTGeneric.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+//kotlin泛型：类型参数化，引入给强类型编程语言加入更强的灵活性
+//泛型可以用于架构开发、开发框架、增加健壮性ClassCastException、理解与研究框架、可以造轮子
+fun main() {
+    //1.泛型接口
+    
+}
--- otherproject/app/src/main/java/com/example/myapplication/KTLoop.kt
+++ otherproject/app/src/main/java/com/example/myapplication/KTLoop.kt
@@ -11,8 +11,41 @@ fun main() {
     items.forEach { item ->
         println("forEach:${item}")
     }
-
-    items.forEachIndexed { index, item->
+    //map list 数组都可以使用
+    items.forEachIndexed { index, item ->
         println("forEachIndexed: ${index},${item}")
-    }///11
+    }
+    //while循环
+    var index = 0
+    while (index < items.size) {
+        println("The $index element is ${items[index++]}") //普通while循环
+    }
+    index = 0
+    //do while先执行一次循环体，再看循环条件while里面是否满足-若满足继续执行循环体，不满足则推出
+    do {
+        println("The $index element is ${items[index++]}") //do while循环 以上两种方式都是一样的输出结果
+    } while (index < items.size)
+
+    //迭代器区间表达式循环控制
+    //for in ..
+    //in 包含左边右边最后一个，左闭右闭的区间
+    //输出结果为：1 2 3 4 5 6 7 8 9 10
+    for (it in 1..10) {
+        print("it is $it")
+    }
+    //for in-until
+    //untile 不包含右边最后一个，左闭右开的区间
+    //输出结果为：1 2 3 4 5 6 7 8 9
+    for (i in 1 until 10){
+        println("i is $i")
+    }
+    //for in downto step
+    //downto是指倒叙遍历 10到1
+    //step是指步长，例如会打印10 8 6 4 2 是指下标相差两个
+    for (i in 10 downTo 1 step 2) {
+        println("$i")
+    }
+    //break与continue 与Java是类似的，break退出所有循环，continue是跳出本次循环，下一层的循环跳不出去
+
+
 }
