eaf032223488f13f4c65b198db3ed00b0964d35a
温鑫 <49605281+wenxiy@users.noreply.github.com>
2023-05-30 23:23:32 +0800

    11
    11

--- otherproject/app/src/main/java/com/example/myapplication/KTGeneric.kt
+++ otherproject/app/src/main/java/com/example/myapplication/KTGeneric.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import org.json.JSONObject
+
 //kotlin泛型：类型参数化，引入给强类型编程语言加入更强的灵活性
 //泛型可以用于架构开发、开发框架、增加健壮性ClassCastException、理解与研究框架、可以造轮子
 fun main() {
@@ -11,6 +13,9 @@ fun main() {
     //3.泛型方法：做Json序列化时，返回值时不确定的，是调用的时候传递的泛型确定的
     //类型参数要放在方法名前面   String::class.java代表传递class类型参数
     fromJson<String>("{}", String::class.java)
+    fromJson2<JSONObject>("{}", JSONObject::class.java)
+    fromJson3<User>("{}", User::class.java)
+    //
 }
 
 interface Drink<T> {
@@ -25,18 +30,45 @@ class DrinkApple : Drink<String> {
 
 }
 
-abstract class Color<T>(val t: T){
+class User : JSONObject(), Comparable<User> {
+    override fun compareTo(other: User): Int {
+        return 0
+    }
+
+}
+
+open class Animal
+
+class DogAnimal : Animal()
+
+class CatAnimal : 
+
+abstract class Color<T>(val t: T) {
     abstract fun printColor()
 }
 
-class BlueColor(val color: String) :Color<String>(color) {
+class BlueColor(val color: String) : Color<String>(color) {
     override fun printColor() {
         println("printColor:${color}")
     }
 
 }
+
 //返回值可能为空，就加一个问号
-fun <T> fromJson(json: String, tClass: Class<T>) :T? {
+fun <T> fromJson(json: String, tClass: Class<T>): T? {
     val t: T? = tClass.newInstance()
     return t
+}
+
+//泛型类型的限定-1
+fun <T : JSONObject> fromJson2(json: String, tClass: Class<T>): T? {
+    val t: T? = tClass.newInstance()
+    return t
+}
+
+//泛型类型的限定-1
+//所传递的类型T必须同时满足 where 子句的所有条件，在下面示例中，类型T必须即实现了JSONObject，也实现了Comparable
+fun <T> fromJson3(json: String, tClass: Class<T>): T? where T : JSONObject, T : Comparable<T> {
+    val instance: T? = tClass.newInstance()
+    return instance
 }
